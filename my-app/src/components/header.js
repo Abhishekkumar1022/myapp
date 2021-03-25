415abca85564f313a97f264da0110180626c8b16
Abhishekkumar1022 <ak87726@gmail.com>
2021-03-25 11:33:01 +0530

    Manually creating All the Components

---
+++ my-app/src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const user = {
+    firstName:'Fracis',
+    lastName : 'Jones',
+    age:'16'
+}
+
+const Header = ()=>{
+    return (
+    <div>
+       Header
+    </div>
+    )
+}
+
+export default Header;
